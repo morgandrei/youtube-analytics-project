98caccd0badd304f5551ba92402f3149a9c2cbb3
Andrey <moregun@list.ru>
2023-06-22 13:53:13 +0300

    change channel.py

--- src/channel.py
+++ src/channel.py
@@ -7,8 +7,8 @@ youtube = build('youtube', 'v3', developerKey=api_key)
 
 class Channel:
     """Класс для ютуб-канала"""
-    api_key: str = os.getenv('YT_API_KEY')
-    youtube = build('youtube', 'v3', developerKey=api_key)
+
+
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
