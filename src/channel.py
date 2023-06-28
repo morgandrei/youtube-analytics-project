2e3c01ab2a31d966bd5724e289531b08dcf5636e
Andrey <moregun@list.ru>
2023-06-28 11:51:26 +0300

    домашка 3

--- src/channel.py
+++ src/channel.py
@@ -17,9 +17,40 @@ class Channel:
         self.title = self.__channel["items"][0]["snippet"]["title"]
         self.description = self.__channel["items"][0]["snippet"]["description"]
         self.url = f"https://www.youtube.com/" + self.__channel['items'][0]['snippet']['customUrl']
-        self.subscriber_count = self.__channel["items"][0]["statistics"]["subscriberCount"]
-        self.video_count = self.__channel["items"][0]["statistics"]["videoCount"]
-        self.view_count = self.__channel["items"][0]["statistics"]["viewCount"]
+        self.subscriber_count = int(self.__channel["items"][0]["statistics"]["subscriberCount"])
+        self.video_count = int(self.__channel["items"][0]["statistics"]["videoCount"])
+        self.view_count = int(self.__channel["items"][0]["statistics"]["viewCount"])
+
+    def __str__(self):
+        return str(f"'{self.title} ({self.url})'")
+
+    def __add__(self, highload):
+        """Метод для операции сложения"""
+        return int(self.subscriber_count + highload.subscriber_count)
+
+    def __sub__(self, highload):
+        """Метод для операции вычитания"""
+        return self.subscriber_count - highload.subscriber_count
+
+    def __sub__(self, highload):
+        """Метод для операции вычитания"""
+        return highload.subscriber_count - self.subscriber_count
+
+    def __gt__(self, highload):
+        """Метод для операции сравнения «больше»"""
+        return highload.subscriber_count < self.subscriber_count
+
+    def __ge__(self, highload):
+        """Метод для операции сравнения «больше или равно»"""
+        return highload.subscriber_count <= self.subscriber_count
+
+    def __lt__(self, highload):
+        """Метод для операции сравнения «меньше»"""
+        return highload.subscriber_count > self.subscriber_count
+
+    def __le__(self, highload):
+        """Метод для операции сравнения «меньше или равно»"""
+        return highload.subscriber_count >= self.subscriber_count
 
     @property
     def channel_id(self):
