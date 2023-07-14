88f1f8595022f668b9a1079104d9d6b95ce9ae36
Andrey <moregun@list.ru>
2023-07-14 22:43:37 +0300

    Домашка 6

--- src/video.py
+++ src/video.py
@@ -1,18 +1,34 @@
 from src.channel import youtube
 
 
+class IncorrectId(Exception):
+    def __init__(self, *args, **kwargs):
+        self.message = args[0] if args else 'Видео с таким id не существует'
+
+
 class Video:
     def __init__(self, id_video):
         self.id_video = id_video
-        video_response = youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
-                                               id=self.id_video).execute()
-        self.video_title: str = video_response['items'][0]['snippet']['title']
-        self.video_url: str = f"https://www.youtube.com/watch?v={self.id_video}"
-        self.view_count: int = video_response['items'][0]['statistics']['viewCount']
-        self.like_count: int = video_response['items'][0]['statistics']['likeCount']
+        try:
+
+            self.video_response = youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
+                                                        id=self.id_video).execute()
+            if len(self.video_response["items"]) == 0:
+                raise IncorrectId
+        except IncorrectId as m:
+            print(m)
+            self.title = None
+            self.url = None
+            self.view_count = None
+            self.like_count = None
+        else:
+            self.title: str = self.video_response['items'][0]['snippet']['title']
+            self.video_url: str = f"https://www.youtube.com/watch?v={self.id_video}"
+            self.view_count: int = self.video_response['items'][0]['statistics']['viewCount']
+            self.like_count: int = self.video_response['items'][0]['statistics']['likeCount']
 
     def __str__(self):
-        return self.video_title
+        return self.title
 
 
 class PLVideo(Video):
@@ -23,4 +39,4 @@ class PLVideo(Video):
         self.id_playlist = id_playlist
 
     def __str__(self):
-        return self.video_title
+        return self.title
